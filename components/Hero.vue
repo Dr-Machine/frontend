8101c71ea989f6bf87a9041388d7734165b34c92
Parsa <parsa.esfahanian.7596@gmail.com>
2021-08-30 15:43:17 +0430

    mid navbar

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,19 +1,48 @@
 <template>
-	<div>
-		<hero-content />
-		<!-- <div class="relative -mt-12 lg:-mt-24">
+	<div class="h-screen py-24 bg-dogeblue-primary">
+		<div
+			class="
+				h-full
+				flex flex-col
+				justify-center
+				items-center
+				text-center
+				md:text-center
+			"
+		>
+			<h1 class="my-4 leading-tight text-6xl font-bold">Dr. Machine</h1>
+			<p class="mb-8 leading-tight text-2xl text-gray-400">
+				Intelligent Medical Imaging
+			</p>
 
-		</div> -->
+			<button
+				class="
+					mx-auto
+					my-4
+					px-6
+					py-3
+					font-semibold
+					rounded-lg
+					bg-dogegreen
+					text-md text-dogeblue-primary
+					border-2 border-dogegreen
+					hover:text-white
+					hover:bg-dogeblue-primary
+					hover:border-dogegreen
+					focus:text-white
+					focus:bg-dogeblue-secondary
+					focus:border-dogeblue-secondary
+					lg:mx-0
+				"
+			>
+				Get started!
+			</button>
+		</div>
 	</div>
 </template>
 
 <script>
-import HeroContent from '@/components/HeroContent'
-
 export default {
 	name: 'Hero',
-	components: {
-		'hero-content': HeroContent,
-	},
 }
 </script>
